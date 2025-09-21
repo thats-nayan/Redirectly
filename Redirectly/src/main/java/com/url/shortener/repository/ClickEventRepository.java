f395c19b592fb264be7c915e0184243a36cd5433
nayan <hanandynayan17@gmail.com>
2025-09-21 18:19:56 +0530

    Added url analytics routes

--- Redirectly/src/main/java/com/url/shortener/repository/ClickEventRepository.java
+++ Redirectly/src/main/java/com/url/shortener/repository/ClickEventRepository.java
@@ -1,10 +1,15 @@
 package com.url.shortener.repository;
 
 import com.url.shortener.models.ClickEvent;
+import com.url.shortener.models.UrlMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ClickEventRepository extends JpaRepository<ClickEvent, Long> {
-
+    List<ClickEvent> findByUrlMappingAndClickDateBetween(UrlMapping urlMapping, LocalDateTime startDate, LocalDateTime endDate);
+    List<ClickEvent> findByUrlMappingInAndClickDateBetween(List <UrlMapping> urlMapping, LocalDateTime startDate, LocalDateTime endDate);
 }
