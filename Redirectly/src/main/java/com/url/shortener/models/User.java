a55bbf4b561cf8b93b3f8a32f5fb9eda40627165
nayan <hanandynayan17@gmail.com>
2025-09-27 15:29:37 +0530

    Revert extra change

--- Redirectly/src/main/java/com/url/shortener/models/User.java
+++ Redirectly/src/main/java/com/url/shortener/models/User.java
@@ -13,7 +13,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String email;
     private String username;
     private String password;
